daf297a52ff8e67f3e36f4e9ac83fe86c6d57f21
dhruvfaujdar07 <Dhruv.41014@stu.upes.ac.in>
2026-08-14 21:56:07 +0530

    Add files via upload

---
+++ Q003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+ int length, breadth;
+ scanf("%d %d", &length, &breadth);
+
+ printf("area = %d\n", legth * breadth);
+ printf("perimeter= %d\n",2 * (length + breadth));
+
+    return 0;
+
+}
---
+++ Q004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ float r, area, circumference;
+
+ scanf("%d", &r);
+
+ area = 3.14 * r * r;
+ circumference = 2 * 3.14 * r;
+ printf("area  5.2f, circumference + %.2f, area, circumference");
+ return 0;
+
+}
